6430d1cc2ee63f97db6529a96020f52624ad876b
GeorgeHan <66801343+Quan123tom@users.noreply.github.com>
2026-07-27 18:40:55 +0300

    fixed skew_unit error

--- skew_unit.sv
+++ skew_unit.sv
@@ -1,5 +1,6 @@
 module skew_unit #(
-    parameter DATA_WIDTH = 8
+    parameter DATA_WIDTH = 8,
+    parameter PIPELINE_DEPTH = 2
 ) (
     input  logic clk,
     input  logic rst,
@@ -12,16 +13,16 @@ module skew_unit #(
     generate
         for (i = 1; i < DATA_WIDTH; i++) begin : gen_skew_pipeline
             logic [DATA_WIDTH-1:0] pipe [0:i-1];
-
+            localparam delay = i * PIPELINE_DEPTH;
             always_ff @(posedge clk or posedge rst) begin
                 if (rst) begin
-                    for (int k = 0; k < i; k++) begin
+                    for (int k = 0; k < delay; k++) begin
                         pipe[k] <= '0;
                     end
                 end else begin
                     pipe[0] <= in_data[i];
 
-                    for (int k = 1; k < i; k++) begin
+                    for (int k = 1; k < delay; k++) begin
                         pipe[k] <= pipe[k-1];
                     end
                 end
