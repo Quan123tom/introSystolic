1e70c395bfdfc171f97d3d608f3b3cf14407c4f8
GeorgeHan <66801343+Quan123tom@users.noreply.github.com>
2026-07-27 22:10:46 +0300

    started fifo

--- system_buffers.sv
+++ system_buffers.sv
@@ -1,7 +1,14 @@
 module buffers #(
-    parameters
+        parameter DATA_WIDTH = 64, // because 8 bit num in an 8x8 matrix and i want to fit and shift one row at a time 
+        parameter FIFO_DEPTH = 10 // how deep it is 
 ) (
-    ports
+    input logic clk,
+    input logic rst,
+    input logic write_valid,
+    output logic write_ready,
+    input  logic [DATA_WIDTH-1:0] write_data,
+    
+
 );
     
 endmodule
